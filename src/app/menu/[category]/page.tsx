a1c0412f610a76c6612e8dc6bb2a74bf321fb8f0
kenan551 <kenan.ramazanov1104@gmail.com>
2023-10-30 19:53:00 +0400

    Creating pages

---
+++ src/app/menu/[category]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BurgerPage = () => {
+  return (
+    <div>BurgerPage</div>
+  )
+}
+
+export default BurgerPage
---
+++ src/app/product/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SingleProduct = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default SingleProduct
