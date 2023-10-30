4e13ae27ec3176b7cfa5aebed8b25df9085e60a4
kenan551 <kenan.ramazanov1104@gmail.com>
2023-10-30 21:27:09 +0400

    Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,22 @@
+
 import React from 'react'
+import Menu from './Menu'
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase'>
+        {/*Logo */}
+        <div className='text-xl'>
+            <Link href="/">
+            Massimo
+            </Link>
+        </div>
+        {/*MobileMenu */}
+       <div>
+        <Menu/>
+       </div>
+    </div>
   )
 }
 
