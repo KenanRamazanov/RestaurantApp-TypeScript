b1878de294affa9c29f1f06b703dfe3a96491d32
kenan551 <kenan.ramazanov1104@gmail.com>
2023-11-01 20:17:57 +0400

    Creating Mobile Navbar

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -24,17 +24,18 @@ const Menu = () => {
         <Image src={"/close.png"} alt='' width={20} height={20} onClick={()=>{setOpen(false)}}/>
       )}
 
+      { open &&(
       <div className='bg-red-500 text-white absolute left-0 top-24 w-full h-[calc(100vh-6rem)] flex flex-col gap-8 items-center justify-center text-3xl z-10'>
 {links.map(item =>(
-    <Link href={item.url} key={item.id}>{item.title}</Link>
+    <Link href={item.url} key={item.id} onClick={()=>{setOpen(false)}}>{item.title} </Link>
 ))}
-{ !user ? <Link href="/login">Login</Link> :
+{ !user ? <Link href="/login" onClick={()=>{setOpen(false)}}>Login</Link> :
 (
-<Link href="/orders">Orders</Link>)}
-<Link href='/cart'>
+<Link href="/orders" onClick={()=>{setOpen(false)}}>Orders</Link>)}
+<Link href='/cart' onClick={()=>{setOpen(false)}}>
     <CartIcon/>
 </Link>
-      </div>
+      </div>)}
     </div>
   )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,6 +6,12 @@ import Link from 'next/link'
 const Navbar = () => {
   return (
     <div className='h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase'>
+        <div className='hidden md:flex gap-4'>
+          <Link href='/'>Homepage</Link>
+          <Link href='/'>Menu</Link>
+          <Link href='/'>Contact</Link>
+
+        </div>
         {/*Logo */}
         <div className='text-xl'>
             <Link href="/">
