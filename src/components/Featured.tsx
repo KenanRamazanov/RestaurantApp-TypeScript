7f85cbdd2f8b36f6d080879d6ac684af4625b79e
kenan551 <kenan.ramazanov1104@gmail.com>
2023-11-04 20:42:56 +0400

    Final of Featured page

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import React from 'react'
+import { featuredProducts } from './data'
 
 const Featured = () => {
   return (
@@ -7,19 +8,25 @@ const Featured = () => {
      {/* Wrapper*/}
       <div className='w-max flex'>
      {/* Single Item*/}
-      <div className='w-screen h-[60vh] flex flex-col items-center justify-around p-4'>
+    {
+    featuredProducts.map(item=>(
+    <div key={item.id} className='w-screen h-[60vh] flex flex-col items-center justify-around p-4 hover:bg-fuchsia-50 transition-all duration-300 md:w-[50vw] xl:w-[33vw] xl:h-[90vh]'>
      {/* IMAGE CONTAINER*/}
-     <div className='relative flex-1 w-full'>
-      <Image src="/temporary/p1.png" alt='' fill className='object-contain'/>
+   {item.img &&(
+     <div className='relative flex-1 w-full hover:rotate-[60deg] transition-all duration-500'>
+      <Image src={item.img} alt=''  fill className='object-contain'/>
      </div>
+     )}
      {/* Text CONTAINER*/}
-     <div className='flex-1 flex flex-col gap-4   '>
-      <h1 className='text-xl font-bold uppercase'>Title</h1>
-      <span className='text-xl font-bold'>$123</span>
+     <div className='flex-1 flex items-center justify-center text-center flex-col gap-4   '>
+      <h1 className='text-xl font-bold uppercase xl:text-2xl 2xl:text-3xl '>{item.title}</h1>
+      <p className='p-4 2xl:p-8'>{item.desc}</p>
+      <span className='text-xl font-bold'>${item.price}</span>
       <button className='bg-red-500 text-white p-2 rounded-md'>Add to Cart</button>
-      <p>Description</p>
+      
      </div>
       </div>
+      ))}
       </div>
     </div>
   )
