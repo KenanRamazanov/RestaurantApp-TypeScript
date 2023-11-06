4d38afa324c369fb29d94037b1a28eb5a8e01d4a
kenan551 <kenan.ramazanov1104@gmail.com>
2023-11-06 21:34:38 +0400

    Creating Countdown

---
+++ src/components/CountDown.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CountDown = () => {
+  return (
+    <div>CountDown</div>
+  )
+}
+
+export default CountDown
--- src/components/Offer.tsx
+++ src/components/Offer.tsx
@@ -5,7 +5,12 @@ const Offer = () => {
   return (
     <div className='bg-black h-screen flex flex-col md:flex-row'>
      {/* Text Container*/}
-   <div className='flex-1'></div>
+   <div className='flex-1 flex flex-col justify-center items-center text-center gap-8 p-6'>
+    <h1 className='text-white text-5xl font-bold xl:text-6xl'>Delicious Burger & French Fry</h1>
+    <p className='text-white xl:text-xl'>Progressively simplify effective e-toilers and process-centric methods
+          of empowerment. Quickly pontificate parallel.</p>
+    <button className='bg-red-500 text-white rounded-md py-3 px-6'>Order Now</button>
+   </div>
      {/* Image Container*/}
    <div className='flex-1 w-full relative'>
 <Image src="/offerProduct.png" alt='' fill className='object-contain'/>
