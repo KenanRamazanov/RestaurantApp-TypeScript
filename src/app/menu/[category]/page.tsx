d0d07993779c8b633faead5b992765ee08b58cb6
kenan551 <kenan.ramazanov1104@gmail.com>
2023-11-10 21:33:10 +0400

    Creating ProductDetail

--- src/app/menu/[category]/page.tsx
+++ src/app/menu/[category]/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { pizzas } from '@/components/data'
 import Image from 'next/image'
 import Link from 'next/link'
--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,8 +1,29 @@
+import { singleProduct } from '@/components/data'
+import Image from 'next/image'
 import React from 'react'
 
 const SingleProduct = () => {
   return (
-    <div>SingleProduct</div>
+    <div className='p-4 lg:px-20 xl:px-40 h-screen flex flex-col justify-around text-red-500 md:flex-row'>
+      {/* Image Container */}
+   { singleProduct && (
+      <div className='relative'>
+        <Image
+        src={singleProduct.img}
+         alt=''
+          className='object-contain' 
+          fill/>
+      </div>
+      )
+      }
+      {/* Text Container */}
+      <div className=''>
+        <h1>{singleProduct.title}</h1>
+        <p>{singleProduct.desc}</p>
+        
+      </div>
+
+    </div>
   )
 }
 
