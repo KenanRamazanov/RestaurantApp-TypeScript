c5c2acde0dc02b9184a8429f2f1294d006f06603
kenan551 <kenan.ramazanov1104@gmail.com>
2023-11-11 12:28:33 +0400

    Final of DetailingPage

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,3 +1,4 @@
+
 import Price from '@/components/Price'
 import { singleProduct } from '@/components/data'
 import Image from 'next/image'
@@ -7,7 +8,7 @@ import React from 'react'
 const SingleProduct = () => {
   
   return (
-    <div className='p-4 lg:px-20 xl:px-40 h-screen flex flex-col justify-around text-red-500 md:flex-row'>
+    <div className='p-4 lg:px-20 xl:px-40 h-screen flex flex-col justify-around text-red-500 md:flex-row md:gap-8 md:items-center'>
       {/* Image Container */}
       {singleProduct.img && (
         <div className="relative w-full h-1/2 md:h-[70%]">
@@ -20,8 +21,8 @@ const SingleProduct = () => {
         </div>
       )}
       {/* Text Container */}
-      <div className='h-1/2 flex flex-col gap-4'>
-        <h1 className='text-3xl font-bold uppercase'>{singleProduct.title}</h1>
+      <div className='h-1/2 flex flex-col gap-4 md:h-[70%] md:justify-center md:gap-6 xl:gap-8'>
+        <h1 className='text-3xl font-bold uppercase xl:text-5xl'>{singleProduct.title}</h1>
         <p>{singleProduct.desc}</p>
         <Price price={singleProduct.price} id={singleProduct.id} options={singleProduct.options}/>
       </div>
