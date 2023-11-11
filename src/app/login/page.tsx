6dbd071fea6920b9d2c73bd762103b48b6959670
kenan551 <kenan.ramazanov1104@gmail.com>
2023-11-12 00:39:31 +0400

    small changes

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,21 +1,21 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const LoginPage = () => {
   return (
-    <div className='p-4 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex items-center justify-center"'>
+    <div className="p-4 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex items-center justify-center">
       {/* BOX */}
-      <div className='h-full shadow-2xl rounded-md flex flex-col md:flex-row md:h-[70%] md:w-full lg:w-[60%] 2xl:w-1/2'>
-     {/* Image Container */}
-     <div className='relative h-1/3 w-full md:h-full md:w-1/2'>
-      <Image src="/loginBg.png" alt='' fill className='object-cover'/>
-     </div>
-     {/* Form Container */}
-     <div className='p-10 flex flex-col gap-8  md:w-1/2'>
-      <h1 className='font-bold text-xl'>Welcome</h1>
-      <p>Log into your account or create a new one using social buttons</p>
-      <button className="flex gap-4 p-4 ring-1 ring-orange-100 rounded-md">
+      <div className=" h-full shadow-2xl rounded-md flex flex-col md:flex-row md:h-[70%] md:w-full lg:w-[60%] 2xl:w-1/2">
+        {/* IMAGE CONTAINER */}
+        <div className="relative h-1/3 w-full md:h-full md:w-1/2">
+          <Image src="/loginBg.png" alt="" fill className="object-cover"/>
+        </div>
+        {/* FORM CONTAINER */}
+        <div className="p-10 flex flex-col gap-8 md:w-1/2">
+          <h1 className="font-bold text-xl xl:text-3xl">Welcome</h1>
+          <p>Log into your account or create a new one using social buttons</p>
+          <button className="flex gap-4 p-4 ring-1 ring-orange-100 rounded-md">
             <Image
               src="/google.png"
               alt=""
@@ -38,11 +38,10 @@ const LoginPage = () => {
           <p className="text-sm">
             Have a problem?<Link className="underline" href="/"> Contact us</Link>
           </p>
-     </div>
-
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
