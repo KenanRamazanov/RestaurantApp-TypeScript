31e4d6b4a0ebdc988c2b7e6ee7e05739387d52ac
kenan551 <kenan.ramazanov1104@gmail.com>
2023-11-12 01:34:58 +0400

    Final Commit

--- src/components/CountDown.tsx
+++ src/components/CountDown.tsx
@@ -1,7 +1,7 @@
 "use client"
 import React from 'react'
 import Countdown from 'react-countdown'
-const endingDate = new Date("2023-12-31")
+const endingDate = new Date("2024-08-31")
 
 const CountDown = () => {
   return (
